c1cc7e7a1677d0105006df744d9a9891e4602cb8
Valentino249 <valentino.vinod@gmail.com>
2026-05-24 15:22:16 -0400

    Button on landing now scrolls to the body on click

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,37 +6,40 @@ import bg from '../assets/background_1.png'
 function Header() {
     return (
         <div>
-            <div className="flex md:items-start justify-center min-h-screen flex-col p-5">
-                <p className="mb-2 text-4xl font-bold text-bg-orange-50 mt-50 mb-10 gap-10 md:px-10">Game Development</p>
-                <div className="grid md:grid-cols-4 grid-cols-1 items-center gap-10 md:px-10">
-                    <div className="flex flex-col">
-                        <a href="https://gold249.itch.io/gravity-jump" target="_blank" rel="noopener noreferrer">
-                            <img src={gravityJump} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
-                        </a>
-                        <p className="mt-5 font-bold text-2xl">Gravity Jump</p>
-                    </div>
-                    
-                    <div className="flex flex-col">
-                        <a href="https://gold249.itch.io/roll-of-the-wreck" target="_blank" rel="noopener noreferrer">
+            <section id="projects">
+                <div className="flex md:items-start justify-center min-h-screen flex-col p-5">
+                    <p className="mb-2 text-4xl font-bold text-bg-orange-50 mt-50 mb-10 gap-10 md:px-10">Game Development</p>
+                    <div className="grid md:grid-cols-4 grid-cols-1 items-center gap-10 md:px-10">
+                        <div className="flex flex-col">
+                            <a href="https://gold249.itch.io/gravity-jump" target="_blank" rel="noopener noreferrer">
+                                <img src={gravityJump} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
+                            </a>
+                            <p className="mt-5 font-bold text-2xl">Gravity Jump</p>
+                        </div>
+                        
+                        <div className="flex flex-col">
+                            <a href="https://gold249.itch.io/roll-of-the-wreck" target="_blank" rel="noopener noreferrer">
 
-                        <img src={rotr} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
-                        </a>
-                        <p className="mt-5 font-bold text-2xl">Roll of The Wreck</p>
+                            <img src={rotr} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
+                            </a>
+                            <p className="mt-5 font-bold text-2xl">Roll of The Wreck</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <a href="https://gold249.itch.io/whats-above" target="_blank" rel="noopener noreferrer">
+                                <img src={whatsAbove} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
+                            </a>
+                            <p className="mt-5 font-bold text-2xl">What's Above</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <img src={bg} alt="Profile Photo" className="md:size-96 size-72 object-cover" />
+                            <p className="mt-5 font-bold text-2xl">Coming soon...</p>
+                        </div> 
                     </div>
-                    <div className="flex flex-col">
-                        <a href="https://gold249.itch.io/whats-above" target="_blank" rel="noopener noreferrer">
-                            <img src={whatsAbove} alt="Profile Photo" className="md:size-96 size-72 object-cover hover:scale-105 transition duration-300" />
-                        </a>
-                        <p className="mt-5 font-bold text-2xl">What's Above</p>
-                    </div>
-                    <div className="flex flex-col">
-                        <img src={bg} alt="Profile Photo" className="md:size-96 size-72 object-cover" />
-                        <p className="mt-5 font-bold text-2xl">Coming soon...</p>
-                    </div> 
+                    <p className="mb-2 text-4xl font-bold text-bg-orange-50 mt-20 mb-10 gap-10 md:px-10">How I Work</p>
+                    <p className="text-2xl text-bg-orange-50 mb-5 gap-10 md:px-10">I got my start programming in high school by developing small video games in python. I improved my game development skills over the years by participating in Game Jams, which taught me to think critically under high pressure. At university, I have worked two internships as a full stack software developer. At my first internship, I worked on a small team of three developers on implementing cost-saving measures and increasing accessibility to CEED's Ruby on Rails website (makerepo.com). I approached each new project at this internship with a careful balance of patience and my learned abilities to work under time constraints. At my second internship with the National Research Council of Canada, I further developed my full stack software development skills by completing the development of a React webform. On this project, designing appealing, accessible front-facing pages was of the utmost importance, as that was what would encourage researchers to use our tool.</p>
                 </div>
-                <p className="mb-2 text-4xl font-bold text-bg-orange-50 mt-20 mb-10 gap-10 md:px-10">How I Work</p>
-                <p className="text-2xl text-bg-orange-50 mb-5 gap-10 md:px-10">I got my start programming in high school by developing small video games in python. I improved my game development skills over the years by participating in Game Jams, which taught me to think critically under high pressure. At university, I have worked two internships as a full stack software developer. At my first internship, I worked on a small team of three developers on implementing cost-saving measures and increasing accessibility to CEED's Ruby on Rails website (makerepo.com). I approached each new project at this internship with a careful balance of patience and my learned abilities to work under time constraints. At my second internship with the National Research Council of Canada, I further developed my full stack software development skills by completing the development of a React webform. On this project, designing appealing, accessible front-facing pages was of the utmost importance, as that was what would encourage researchers to use our tool.</p>
-            </div>
+            </section>
+            
         </div>
     )
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,15 @@ function Header() {
                     <h1 className="mb-2 text-3xl font-bold text-gray-900"><span className="text-red-400">Hi,</span> I'm Valentino Vinod</h1>
                     <p className="mb-6 text-gray-900">I'm a full stack software developer based in Ottawa, Ontario with strong technical skils and a passion for creating video games on the side. Explore my projects below, or check out my professional experience by clicking on one of these useful links.</p>
                     <div className="flex justify-center space-x-5">
-                    
-                    <button className="flex w-full items-center justify-center gap-2 rounded-2xl bg-white p-5 py-3 font-semibold">
+                    <a href="#projects" className="w-full">
+                        <button className="flex w-full items-center justify-center gap-2 rounded-2xl bg-white p-5 py-3 font-semibold">
                         See Projects
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
                     </button>
+                    </a>
+                    
                     </div>
                 </div>
                 <div>
